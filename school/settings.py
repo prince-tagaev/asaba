c880c73296fb792d4942389fda50267bc9fcf979
prince-tagaev <asaba@internet.ru>
2021-03-30 18:37:42 +0600

    Install Pillow and update settings setting static url therd time

--- school/settings.py
+++ school/settings.py
@@ -120,7 +120,7 @@ STATIC_URL = 'blog/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "/blog/static/")
+    os.path.join(BASE_DIR, "/static/")
 ]
 
 
