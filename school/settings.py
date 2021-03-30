2dbe7212bd6de8ff1b6e1da96f3b02965f8f2c7b
prince-tagaev <asaba@internet.ru>
2021-03-30 18:33:52 +0600

    Install Pillow and update settings setting static url

--- school/settings.py
+++ school/settings.py
@@ -115,8 +115,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "blog/static/")
+STATIC_URL = 'static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [
